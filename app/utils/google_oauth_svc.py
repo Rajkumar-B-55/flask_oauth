b64e2da1fbf4d0b27c7364a5ccb018e339ce6c31
rk <rajkumar.rk2403@gmail.com>
2023-08-30 07:42:46 +0530

    Google signup flow changes v2.1

--- app/utils/google_oauth_svc.py
+++ app/utils/google_oauth_svc.py
@@ -1,6 +1,6 @@
 import google.auth.transport.requests
 import requests
-from flask import redirect, session, abort
+from flask import session, abort
 from google.oauth2 import id_token
 
 from config import config
@@ -9,7 +9,7 @@ from config import config
 class GoogleSvcAPI:
     CLIENT_ID = config.GOOGLE_CLIENT_ID
     CLIENT_SECRET = config.GOOGLE_CLIENT_SECRET
-    GOOGLE_REDIRECT_URI = config.GOOGLE_REDIRECT_URI  # http://127.0.0.1:5050/callback
+    GOOGLE_REDIRECT_URI = config.GOOGLE_REDIRECT_URI
 
     # SCOPE  = ['']
 
@@ -26,29 +26,27 @@ class GoogleSvcAPI:
 
     @classmethod
     def callback(cls, code):
-        token_url = "https://oauth2.googleapis.com/token"
-        token_payload = {
-            "code": code,
-            "client_id": cls.CLIENT_ID,
-            "client_secret": cls.CLIENT_SECRET,
-            "redirect_uri": cls.GOOGLE_REDIRECT_URI,
-            "grant_type": "authorization_code",
-        }
+        try:
+            token_url = "https://oauth2.googleapis.com/token"
+            token_payload = {
+                "code": code,
+                "client_id": cls.CLIENT_ID,
+                "client_secret": cls.CLIENT_SECRET,
+                "redirect_uri": cls.GOOGLE_REDIRECT_URI,
+                "grant_type": "authorization_code",
+            }
 
-        response = requests.post(token_url, data=token_payload)
-        token_data = response.json()
+            response = requests.post(token_url, data=token_payload)
+            token_data = response.json()
 
-        id_info = id_token.verify_oauth2_token(
-            id_token=token_data["id_token"],
-            request=google.auth.transport.requests.Request(),
-            audience=cls.CLIENT_ID,
-        )
-        return id_info
-
-    @classmethod
-    def logout(cls):
-        session.clear()
-        return redirect("/")
+            id_info = id_token.verify_oauth2_token(
+                id_token=token_data["id_token"],
+                request=google.auth.transport.requests.Request(),
+                audience=cls.CLIENT_ID,
+            )
+            return id_info
+        except Exception as e:
+            raise e
 
     @classmethod
     def login_is_required(cls, function):
